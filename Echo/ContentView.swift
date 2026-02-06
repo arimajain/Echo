6f619abb4ef49e952c2f6edb2fd8c6627cc3bd74
arimajain <arimajain110205@gmail.com>
2026-02-06 13:42:26 +0530

    Initial Commit

---
+++ Echo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Echo
+//
+//  Created by Ari on 06/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
